033dd1985203e362da4e6d1fca38ff46192ea63e
Pedro Ariztia <ppariztia1@miuandes.cl>
2025-09-25 14:46:43 -0300

    Asociaciones listas

--- db/migrate/20250925172553_add_asociation_guest_reservation.rb
+++
@@ -1,8 +0,0 @@
-class AddAsociationGuestReservation < ActiveRecord::Migration[8.0]
-  def change
-    change_table :reservations do |t|
-      t.references :guest, null: false, foreign_key: true
-      t.references :room, null: false, foreign_key: true
-    end
-  end
-end
